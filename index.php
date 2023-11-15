c86fd2fbb2d45e6c1d094096345ac73c3ddd0781
ovarz <ovarz_03@yahoo.com>
2023-11-15 19:49:07 +0700

    otogear

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-otogear
+<html>
+<head>
+  <link href="img/favicon.ico" rel="icon" type="image/ico" />
+</head>
+<style>
+  *,html{padding:0;margin:0;}
+  html{background-color:#000000;}
+  body{display:flex; align-items:center; justify-content:center; padding:34px;}
+  img{width:100%; max-width:344px;}
+</style>
+
+<body>
+  <img src="img/logo.png"/>
+</body>
+</html>
