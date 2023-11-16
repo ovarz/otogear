0602c9e658606025aab4422ecd731f51e676bd61
ovarz <ovarz_03@yahoo.com>
2023-11-17 01:54:50 +0700

    Update base.php

--- inc/base.php
+++ inc/base.php
@@ -3,8 +3,8 @@
 <base href="/" />
 <?php 
   $_SERVER['OG'] = $_SERVER['DOCUMENT_ROOT'] . '/'; 
-  $anticache = date ('s'.'i'.'H'.'d'.'m'.'Y');
-  //$anticache = '1711230202';
+  //$anticache = date ('s'.'i'.'H'.'d'.'m'.'Y');
+  $anticache = '1711230155';
   $site_name = 'OtoGear';
   $site_title = 'OtoGear | Channel yang suka bahas-bahas ringan soal Otomotif.';
   $site_desc = 'Otogear menyajikan video informasi seputar otomotif terkini, test, review, harga, tips, modifikasi, dan komunitas.';
